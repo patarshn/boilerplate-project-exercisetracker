ccb04a7fabcd2fef907a4188d4075d14527816e7
Patar Martua Doli Siahaan <38348364+patarshn@users.noreply.github.com>
2022-06-26 17:00:01 +0000

    add schema

--- index.js
+++ index.js
@@ -9,9 +9,48 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+/////////////////////////////////////
+
+//Set Mongoose
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const { Schema } = mongoose;
+
+
+const urlSchema = new Schema({
+  original_url : { type: String, required: true },
+  short_url  : { type: String, required: true },
+});
+
+const userSchema = new Schema({
+  username : { type : String, required: true },
+});
+const exerciseSchema = new Schema({
+  username: { type : String, required: true },
+  description: { type : String, required: true },
+  duration: { type : Number, required: true },
+  date: { type: Date, default: Date.now },
+});
+// const logSchema = new Schema({
+//      username: "fcc_test",
+//     count: 1,
+//     _id: "5fb5853f734231456ccb3b05",
+//     log: [{
+//       description: "test",
+//       duration: 60,
+//       date: "Mon Jan 01 1990",
+//     }]
+// });
+
+let Url = mongoose.model('Url', urlSchema);
 
 
 
+
+
+
+
+/////////////////////////////////////
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
